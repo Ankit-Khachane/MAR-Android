50fdcdd5c3f5b512dfb6b04c8748a2630a763b03
Ankit Khachane <khachane.ankit417@gmail.com>
2019-02-06 23:27:39 +0530

    Added Android Util Library, App Class,Preference Persistence Class

---
+++ app/src/main/java/com/mar/Preference.java
@@ -0,0 +1,38 @@
+package com.mar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Preference {
+    // String Value Variable for Assigning SharedPreference File Name
+    private static final String PreferenceFileName = "com.mar.MARSharedPreference";
+
+    //Static Key Values For Preferences <Key,Value>
+    private static String AccessUsagePermissionStatus = "AccessUsagePermissionStatus";
+    private static String IsOpenedFirstTime = "IsOpenedFirstTime";
+
+    //SharedPreference Api for Persistent data consumption
+    private static SharedPreferences mSharedPreferences;
+    private static SharedPreferences.Editor mEditor;
+
+    public Preference(Context context) {
+        mSharedPreferences = context.getSharedPreferences(PreferenceFileName, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+    }
+
+    public static Boolean getAccessUsagePermissionStatus() {
+        return mSharedPreferences.getBoolean(AccessUsagePermissionStatus, false);
+    }
+
+    public static void setAccessUsagePermissionStatus(Boolean accessUsagePermissionStatus) {
+        mEditor.putBoolean(AccessUsagePermissionStatus, accessUsagePermissionStatus).commit();
+    }
+
+    public static Boolean getIsOpenedFirstTime() {
+        return mSharedPreferences.getBoolean(IsOpenedFirstTime, false);
+    }
+
+    public static void setIsOpenedFirstTime(Boolean isOpenedFirstTime) {
+        mEditor.putBoolean(IsOpenedFirstTime, isOpenedFirstTime).commit();
+    }
+}
